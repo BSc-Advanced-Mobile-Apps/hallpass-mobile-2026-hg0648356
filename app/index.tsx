fcbe1d5b5a5aa087ea0fd8eeca5957171cbdc189
hg0648356 <0648356@hc.ac.je>
2026-03-19 09:31:16 +0000

    acted on feedback - task line to edge of screen

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,6 @@ import { Checkbox } from '@/components/ui/checkbox';
 export default function HomeScreen() {
   return (
     <View className="bg-background flex flex-1 py-32">
-      <Text className="text-center text-white">Hello, world!</Text>
       <View className="flex flex-row">
         <View className="flex items-center justify-center p-4">
           <Checkbox
@@ -13,7 +12,7 @@ export default function HomeScreen() {
             className="border-foreground-transparent h-4 w-4 rounded-xs border-2"
           />
         </View>
-        <View className="border-b-foreground-transparent w-100 border py-2">
+        <View className="border-b-foreground-transparent w-100 flex-1 border py-2">
           <Text className="text-foreground">Subject Assignment</Text>
           <Text className="text-foreground-transparent">Due: Oct 20</Text>
         </View>
